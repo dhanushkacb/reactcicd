9b137e827fc619ceb4c7fc97b0871462c6662806
Dhanushka.Bandara <Dhanushka.Bandara@acumatica.com>
2023-02-16 12:32:32 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with me!
+          Learn React with me new commit!
         </a>
       </header>
     </div>
